d9a5ed529108647912a5112fdbc5c064a1623154
tds3 <tds3@0f7b2756-1b0e-4146-bea1-ebcb1606505b>
2016-01-12 22:52:54 +0000

    adding directory for scratch parametric surface cod

---
+++ code/matlab_tutorial/3DSurfacePDEMatlab/parametric_scratch/torus_parameterization.m
@@ -0,0 +1,10 @@
+% torus_parameterization.m	% jan 12, 2016
+
+N_theta = 10;
+N_phi   = 10;
+theta = linspace(-pi,pi,N_theta);
+phi   = linspace(-pi,pi,N_phi);
+
+xi = @(theta,phi) [ ,...
+                    ,...
+	            ]]
